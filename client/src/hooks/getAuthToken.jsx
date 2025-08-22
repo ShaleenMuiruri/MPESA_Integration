92488a08ace197a0d71f0b209a95460b1987c0eb
Shaleen <shaleengasherry@gmail.com>
2025-08-22 03:27:10 +0300

    Updating Auth Endpoint

--- client/src/hooks/getAuthToken.jsx
+++ client/src/hooks/getAuthToken.jsx
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 export const getAuthToken = async () => {
+  const BaseURL = (import.meta).env?.API_BASE_URL || "";
+
   try {
-    const res = await axios.get(`/api/payments/token`);
+    const res = await axios.get(`${BaseURL}/api/payments/token`);
     const data = res.data;
     if (data?.success && data?.auth_token) {
       localStorage.setItem("mpesa_access_token", data.auth_token);
-    }
+    }ƒ
     return data.auth_token;
   } catch (err) {
     console.error("Error fetching token", err);
